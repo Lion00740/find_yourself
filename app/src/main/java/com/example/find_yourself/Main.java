e45c77991bb393f3b67446907475c69700a72b2f
Papshev Nikita <nikitapap@yandex.ru>
2024-01-10 15:19:25 +0300

    Start development

---
+++ app/src/main/java/com/example/find_yourself/Main.java
@@ -0,0 +1,26 @@
+package com.example.find_yourself;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main.this, Tests.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/find_yourself/Tests.java
@@ -0,0 +1,17 @@
+package com.example.find_yourself;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Tests extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tests);
+        Button test1 = findViewById(R.id.test1);
+        Button go_back = findViewById(R.id.go_back);
+    }
+
+}
